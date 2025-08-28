2b6d68b8bf6ed254eac6bc87acd605fc932c706c
Hari Singh Joshi <jiffymenu@gmail.com>
2025-08-29 03:10:13 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🚀 Running Alembic migrations..."
-alembic upgrade head
-
-echo "✅ Starting FastAPI with Gunicorn + Uvicorn workers..."
-exec gunicorn -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:8000 --workers 4 --timeout 60
